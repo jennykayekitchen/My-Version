2c8a5e6d07e6e231d9f551f483cb82395a747929
Jenny Kitchen <jennykayekitchen@gmail.com>
2023-01-31 14:46:30 -0600

    save tag to song

---
+++ src/components/MusicCatalog/Song.js
@@ -0,0 +1,60 @@
+import { useEffect, useState } from "react"
+//import { useNavigate } from "react-router-dom"
+
+export const Song = ({ songName, albumName, artist, featuringName, songId, tags }) => {
+    const localMyVersionUser = localStorage.getItem("my_version_user")
+    const myVersionUserObject = JSON.parse(localMyVersionUser)
+    
+    const [taggedSong, setTaggedSong] = useState({
+        userId: myVersionUserObject.id,
+        songId: songId,
+        tagId: 0
+    })
+
+    const handleSaveButtonClick = (event) => {
+        //event.preventDefault()
+        
+      
+   
+        return fetch(`http://localhost:8088/taggedSongs`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+                body: JSON.stringify(taggedSong)
+        })
+
+    }
+            
+
+
+    return <>
+    <div className="song">
+                    <div className="songName">Track Name: {songName}</div>
+                    <div className="albumName">Album: {albumName}</div>
+                    <div className="artist">Artist: {artist}</div>
+                    <div className="featuring">Featuring: {featuringName}</div>
+                    <select className="tagDropdown" onChange={
+                                    (event) => {
+                                        const copy = {...taggedSong}
+                                        copy.tagId = parseInt(event.target.value)
+                                        setTaggedSong(copy)
+                                    }
+                                }>
+                        <option value="0" >Select Tag</option>
+                        {tags.map(
+                            (tag) => {
+                                return <option key={tag.id} 
+                                value={tag.id}
+                                >{tag.name}</option>
+                            }
+                        )}
+                    </select>
+                    <button 
+                        onClick={(clickEvent) => handleSaveButtonClick(clickEvent)}
+                            className="btn btn-primary">
+                            Save Tag
+                    </button>
+                    </div>
+    </>
+}
--- src/components/MusicCatalog/SongList.js
+++ src/components/MusicCatalog/SongList.js
@@ -1,28 +1,29 @@
 import { useEffect, useState } from "react"
+import { useParams, useNavigate } from "react-router-dom"
+import { Song } from "./Song"
 //import { Song } from "./Song"
 
 import "./SongList.css"
 
 export const SongList = () => {
     const [songs, setSongs] = useState([])
-
     useEffect(
         () => {
             fetch(`http://localhost:8088/songs`)
                 .then(response => response.json())
                 .then((data) => {
                     setSongs(data)
+                    
                 })                
         },
         []
     )
 
     const [tags, setTags] = useState([])
-
     useEffect(
         () => {
             fetch(`http://localhost:8088/tags`)
-            .then(response => response.json())
+            .then(response =>response.json())
             .then((data) => {
                 setTags(data)
             })
@@ -33,28 +34,14 @@ export const SongList = () => {
     return <div className="songs">
         <h2>Music Catalog</h2>
         {
-            songs.map(
-                (song) => {
-                    return (
-                    <div className="song" key={`song--${song.songId}`}>
-                    <div className="songName">Track Name: {song.songName}</div>
-                    <div className="albumName">Album: {song.albumName}</div>
-                    <div className="artist">Artist: {song.artist}</div>
-                    <div className="featuring">Featuring: {song.featuringName}</div>
-                    <select className="tagDropdown">
-                        <option value="0">Select Tag</option>
-                        {tags.map(
-                            (tag) => {
-                                return <option key={tag.id} value={tag.id}>{tag.name}</option>
-                            }
-                        )}
-                    </select>
-                    </div>
-                    )
-                }
-            )
-        }
-        
+            songs.map(song => <Song key={`song--${song.id}`} songId={song.id}
+                songName = {song.songName}
+                albumName = {song.albumName}
+                artist = {song.artist}
+                featuringName = {song.featuringName}
+                tags = {tags}
+                />)       
+            }        
     </div>
 }
 
