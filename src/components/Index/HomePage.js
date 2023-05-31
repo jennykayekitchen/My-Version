2bf594faa2ea5c4f0b35c868ff26396227cbd776
Jenny Kitchen <jennykayekitchen@gmail.com>
2023-05-31 08:47:25 -0500

    gif and cards adjusted for mobile layout

--- src/components/Index/HomePage.js
+++ src/components/Index/HomePage.js
@@ -2,6 +2,7 @@ import "./HomePage.css"
 
 export const HomePage = () => {
     return <div className="home_page">
+        <div className="mobile-image"></div>
         <div className="container">
             <div className="card1"><div className="cardWords">Step One</div><div className="cardTitle"><a href="/addtag"><h2>Set the Mood</h2></a></div></div>
             <div className="card2"><div className="cardWords">Step Two</div><div className="cardTitle"><a href="/musiccatalog"><h2>Pick the Songs</h2></a></div></div>
